234cd901e9d96c089cb3faa0351f008856d0cbe8
Aymeric <82647012+MrAymeric@users.noreply.github.com>
2022-01-24 09:11:06 +0100

    Create User-info.js

---
+++ User-info.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+
+module.exports.run = async (Client, message, args) => {
+
+    const user = Client.users.cache.get(args[0]) || message.mentions.users.first() || message.author
+
+
+    let embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setAuthor({name: Client.user.tag ,iconURL: Client.user.avatarURL()})
+    .setTitle("User-Info")
+    .setDescription(`Voici les infos de ${user.username}`)
+    .addField("Pseudo : ", `${user.tag}`)
+    .addField("Identifiant : ", `${user.id}`)
+    .addField("Création du compte : ", `${moment(user.createdAt).format("Do MMMM YYYY à hh:mm")}`)
+    .addField("Membre de ce serveur : ", `${moment(message.guild.members.cache.get(user.id).joinedAt).format("Do MMMM YYYY à hh:mm")}`)
+    .setThumbnail(user.avatarURL({ dynamic: true, size: 1024}))
+    .setTimestamp()
+    .setFooter({text: "Demandé par " + message.author.username, iconURL: message.author.avatarURL()});
+                message.channel.send({ embeds : [embed]});
+}
+
+module.exports.config = {
+    name: 'user-info'
+}
